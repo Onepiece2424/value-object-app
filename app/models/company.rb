09f165bcf7c5d23f666bbc2673616a166fba0b90
rioto.nagano <kingdom950927@gmail.com>
2024-08-10 08:49:46 +0900

    fix:class_nameをPostCodeに変更

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,6 @@
 class Company < ApplicationRecord
   composed_of :post_code,
-              class_name: 'AddressPostCode',
+              class_name: 'PostCode',
               mapping: %w(post_code value)
 
   def post_code_valid?
