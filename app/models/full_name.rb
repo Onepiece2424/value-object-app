ed4cc64ff50443fe0f99ed3c863f952895bdea1b
rioto.nagano <kingdom950927@gmail.com>
2024-08-10 12:10:00 +0900

    feat:フルネームの値オブジェクトを作成

---
+++ app/models/full_name.rb
@@ -0,0 +1,12 @@
+class FullName
+  attr_reader :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def to_s
+    "#{first_name} #{last_name}"
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   composed_of :post_code,
               class_name: 'PostCode',
-              mapping: %w(post_code value),
-              constructor: ->(value) { PostCode.new(value) }
+              mapping: %w(post_code value)
+  composed_of :full_name,
+              class_name: 'FullName',
+              mapping: [%w(first_name first_name), %w(last_name last_name)]
 
   def post_code_valid?
     post_code.valid?
