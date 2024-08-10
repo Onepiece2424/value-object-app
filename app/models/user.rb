0dd94f4772afa4ae8297e0eb1cc0406381f9570c
rioto.nagano <kingdom950927@gmail.com>
2024-08-10 10:36:17 +0900

    feat:Userモデルに名前を管理するためのカラムを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   composed_of :post_code,
               class_name: 'PostCode',
-              mapping: %w(post_code value)
+              mapping: %w(post_code value),
+              constructor: ->(value) { PostCode.new(value) }
 
   def post_code_valid?
     post_code.valid?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_08_09_214457) do
+ActiveRecord::Schema.define(version: 2024_08_10_013418) do
 
   create_table "companies", force: :cascade do |t|
     t.integer "post_code"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 2024_08_09_214457) do
     t.integer "post_code"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 
 end
