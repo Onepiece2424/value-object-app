a4a753ba76aac449222db7f8d8d9008d42185773
rioto.nagano <kingdom950927@gmail.com>
2024-08-12 10:47:03 +0900

    feat:user,companyの一覧ページの作成

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,10 @@
 class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+  end
+
+  private
+  def companies_params
+    params.require(:company).permit(:post_code)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  private
+  def users_params
+    params.require(:user).permit(:post_code)
+  end
 end
