9074451c85452f3071ee478ae737a774259005f3
rioto.nagano <kingdom950927@gmail.com>
2024-08-12 12:32:16 +0900

    feat:composed_of をコメントアウト

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,7 +1,7 @@
 class Company < ApplicationRecord
-  composed_of :post_code,
-              class_name: 'PostCode',
-              mapping: %w(post_code value)
+  # composed_of :post_code,
+  #             class_name: 'PostCode',
+  #             mapping: %w(post_code value)
 
   def post_code_valid?
     post_code.valid?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  composed_of :post_code,
-              class_name: 'PostCode',
-              mapping: %w(post_code value)
+  # composed_of :post_code,
+  #             class_name: 'PostCode',
+  #             mapping: %w(post_code value)
   composed_of :full_name,
               class_name: 'FullName',
               mapping: [%w(first_name first_name), %w(last_name last_name)]
