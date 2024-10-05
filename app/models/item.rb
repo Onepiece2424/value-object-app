728aad7c16293f578988268793ea5d029bc226bf
rioto.nagano <kingdom950927@gmail.com>
2024-10-05 13:25:57 +0900

    refactor:不要な部分を削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ApplicationRecord
-  has_many :production_dates, inverse_of: :item, dependent: :destroy
+  has_many :production_dates, dependent: :destroy
   accepts_nested_attributes_for :production_dates, allow_destroy: true
 end
